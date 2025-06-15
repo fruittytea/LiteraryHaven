40b28e1113df034fe72ec0d2f8cee2c6e657ae7f
fruittytea <159552439+fruittytea@users.noreply.github.com>
2025-06-15 16:12:00 +0300

    Update newreadbook.php
    Добавление комментариев

--- public_html/newreadbook.php
+++ public_html/newreadbook.php
@@ -1,4 +1,5 @@
 <?php
+//Начало сессии
 session_start();
 if (isset($_SESSION['acc'])) {
     $acc = $_SESSION['acc']; 
@@ -9,7 +10,7 @@ if (isset($_SESSION['acc'])) {
     header("Location: autorisation.php");
     exit();
 }
-
+//Подключение к БД
 $host = "localhost";
 $dbname = "sadkovaann";
 $password = "R2UJCEw@Q";
@@ -19,7 +20,7 @@ $db_connect = mysqli_connect($host, $user, $password, $dbname);
 if (!$db_connect) {
     die("Ошибка подключения: " . mysqli_connect_error());
 }
-
+//Проверка роли
 $roleCheck = "SELECT Role FROM user WHERE UserId = $acc";
 $roleCheckSql = mysqli_query($db_connect, $roleCheck);
 
@@ -27,18 +28,18 @@ if ($roleCheckSql && mysqli_num_rows($roleCheckSql) > 0) {
     $rowRole = mysqli_fetch_assoc($roleCheckSql);
     $RoleId = $rowRole['Role'];
 }
-
+//Получение данных о книге
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $bname = mysqli_real_escape_string($db_connect, $_POST['user-bname-box']);
     $aname = mysqli_real_escape_string($db_connect, $_POST['user-aname-box']);
     $bdescr = mysqli_real_escape_string($db_connect, $_POST['book-descr-tb']);
     $bgenre = mysqli_real_escape_string($db_connect, $_POST['sel-genre']);
-    
+    //Проверка на отметку об изменении и получение кода книги
     if(isset($_POST['edit']) && isset($_POST['BookId'])) {
         $Edit = $_POST['edit'];
         $EditBookId = $_POST['BookId'];
     }
-
+    //При роди администратора
     if($RoleId == 1){
         $moder = true;
         $path = "library.php";
@@ -54,21 +55,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $path = "library.php?mylib=1";
         $mess = "Книга отправлена на модерацию! После проверки вы сможете найти её в библиотеке!";
     }
-
+    //Действия при изменении
     if($Edit){
+        //Запрос на обновлении данных о книге
         $query = "UPDATE book SET BookName = '$bname', Author ='$aname', Genre = '$bgenre', BookDescription = '$bdescr' WHERE BookId = $EditBookId";
         if (mysqli_query($db_connect, $query)) {
-            // Файл обложки
+            //Файл обложки
             if (isset($_FILES['book-img-tb']) && $_FILES['book-img-tb']['error'] === UPLOAD_ERR_OK) {
                 $fileTmpPath = $_FILES['book-img-tb']['tmp_name'];
                 $fileName = $_FILES['book-img-tb']['name'];
                 $fileNameCmps = explode(".", $fileName);
                 $fileExtension = strtolower(end($fileNameCmps));
-
+                //Типы файлов
                 $allowedfileExtensions = ['jpg', 'jpeg', 'png', 'gif'];
                 if (in_array($fileExtension, $allowedfileExtensions)) {
                     $newFileName = $EditBookId . '.' . $fileExtension;
-
+                    //Папка для хранения
                     $uploadFileDir = 'Images/Books/';
                     if (!is_dir($uploadFileDir)) {
                         mkdir($uploadFileDir, 0755, true);
@@ -76,6 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $dest_path = $uploadFileDir . $newFileName;
 
                     if (move_uploaded_file($fileTmpPath, $dest_path)) {
+                        //Обновление обложки
                         $updateQuery = "UPDATE book SET BookImage = '$newFileName' WHERE BookId = $EditBookId";
                         mysqli_query($db_connect, $updateQuery);
                         echo "<script>alert('".$mess."');
@@ -101,22 +104,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
     else{
+        //Запрос на добавление
         $query = "INSERT INTO book (BookName, Author, BookDescription, Genre, ModerationPassed) VALUES ('$bname', '$aname', '$bdescr', '$bgenre', '$moder')";
         if (mysqli_query($db_connect, $query)) {
-            //Код книги
+            //Код добавленной книги
             $bookId = mysqli_insert_id($db_connect);
 
-            // Файл обложки
+            //Файл обложки
             if (isset($_FILES['book-img-tb']) && $_FILES['book-img-tb']['error'] === UPLOAD_ERR_OK) {
                 $fileTmpPath = $_FILES['book-img-tb']['tmp_name'];
                 $fileName = $_FILES['book-img-tb']['name'];
                 $fileNameCmps = explode(".", $fileName);
                 $fileExtension = strtolower(end($fileNameCmps));
-
+                //Типы файлов
                 $allowedfileExtensions = ['jpg', 'jpeg', 'png', 'gif'];
                 if (in_array($fileExtension, $allowedfileExtensions)) {
                     $newFileName = $bookId . '.' . $fileExtension;
-
+                    //Папка для хранения
                     $uploadFileDir = 'Images/Books/';
                     if (!is_dir($uploadFileDir)) {
                         mkdir($uploadFileDir, 0755, true);
@@ -124,6 +128,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $dest_path = $uploadFileDir . $newFileName;
 
                     if (move_uploaded_file($fileTmpPath, $dest_path)) {
+                        //Обновление обложки добавленной книги
                         $updateQuery = "UPDATE book SET BookImage = '$newFileName' WHERE BookId = $bookId";
                         mysqli_query($db_connect, $updateQuery);
                         echo "<script>alert('".$mess."');
