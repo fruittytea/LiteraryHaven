af1d0ee0d0876de0a3ddad86f1e3cfc1354900be
fruittytea <159552439+fruittytea@users.noreply.github.com>
2025-06-15 19:45:04 +0300

    Update unsubscribe.php
    Добавление комментариев

--- public_html/unsubscribe.php
+++ public_html/unsubscribe.php
@@ -1,4 +1,5 @@
 <?php
+//Начало сессии
 session_start();
 if (!isset($_SESSION['acc'])) {
     echo json_encode(['success' => false, 'message' => 'Не авторизован']);
@@ -12,7 +13,7 @@ if (isset($_POST['userId'])) {
 if(isset($_POST['otherUser'])){
     $bloggerId = $_POST['otherUser'];
 }
-
+//Подключение к БД
 $host = "localhost";
 $dbname = "sadkovaann";
 $password = "R2UJCEw@Q";
@@ -22,13 +23,13 @@ $db_connect = mysqli_connect($host, $user, $password, $dbname);
 if (!$db_connect) {
     die("Ошибка подключения: " . mysqli_connect_error());
 }
-
+//Запрос на удаление подписки
 $query = "DELETE FROM subscription WHERE Subscriber = ? AND Blogger = ?";
 $stmt = mysqli_prepare($db_connect, $query);
 mysqli_stmt_bind_param($stmt, 'ii', $subscriberId, $bloggerId);
 
 if (mysqli_stmt_execute($stmt)) {
-    // Получаем новое количество подписчиков
+    //Получение нового количества подписчиков
     $countQuery = "SELECT COUNT(*) as Fans FROM subscription WHERE Blogger = ?";
     $countStmt = mysqli_prepare($db_connect, $countQuery);
     mysqli_stmt_bind_param($countStmt, 'i', $bloggerId);
