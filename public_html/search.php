f4f42b875bd81478b6fd58181a404f0512a606dc
fruittytea <159552439+fruittytea@users.noreply.github.com>
2025-06-15 19:29:43 +0300

    Update search.php
    Добавление комментариев

--- public_html/search.php
+++ public_html/search.php
@@ -1,14 +1,15 @@
 <?php
+//Начало сессии
 session_start();
 header('Content-Type: application/json');
 $acc = $_SESSION['acc'];
 
+//Подключение к бд
 $host = "localhost";
 $dbname = "sadkovaann";
 $password = "R2UJCEw@Q";
 $user = "sadkovaann";
 
-// Подключение к базе данных
 $db_connect = mysqli_connect($host, $user, $password, $dbname);
 if (!$db_connect) {
     die("Ошибка подключения: " . mysqli_connect_error());
@@ -22,17 +23,18 @@ $mylib = isset($_GET['mylib']) ? intval($_GET['mylib']) : null;
 $genreId = isset($_GET['genre']) ? intval($_GET['genre']) : null;
 $otherUser = isset($_GET['otherUser']) ? intval($_GET['otherUser']) : null;
 
+//Запрос на поиск книг по автору или названию
 $query = "SELECT b.BookId, b.BookName, b.Author, b.BookImage FROM book b 
           WHERE (b.BookName LIKE '%$searchTerm%' OR b.Author LIKE '%$searchTerm%') AND b.ModerationPassed = true";
-
+//Добавление жанра при его выборе
 if ($genreId) {
     $query .= " AND b.Genre = $genreId";
 }
-
+//Добавление условия для поиска в библиотеке другого читателя
 if ($otherUser) {
     $query .= " AND EXISTS (SELECT 1 FROM readbook r WHERE r.User = $otherUser AND r.Book = b.BookId)";
 }
-
+//Добавление условия для поиска в личной библиотеке
 if ($mylib) {
     $query .= " AND EXISTS (SELECT 1 FROM readbook r WHERE r.User = $acc AND r.Book = b.BookId)";
 }
@@ -42,6 +44,7 @@ $query .= " ORDER BY b.AverageScore DESC";
 $result = mysqli_query($db_connect, $query);
 
 $books = [];
+//Формирование массива с книгами
 while ($row = mysqli_fetch_assoc($result)) {
     $books[] = [
         'BookId' => $row['BookId'],
