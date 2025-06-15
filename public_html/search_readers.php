c010a53af3d701b3d1cee0be8f389a0cc3f4fbe3
fruittytea <159552439+fruittytea@users.noreply.github.com>
2025-06-15 19:32:27 +0300

    Update search_readers.php
    Добавление комментариев

--- public_html/search_readers.php
+++ public_html/search_readers.php
@@ -1,4 +1,5 @@
 <?php
+//Начало сессии
 session_start();
 if (isset($_SESSION['acc'])) {
     $acc = $_SESSION['acc'];
@@ -9,6 +10,7 @@ if (isset($_SESSION['acc'])) {
     header("Location: autorisation.php");
     exit();
 }
+//Подключение к БД
 $host="localhost";
 $dbname="sadkovaann";
 $password="R2UJCEw@Q";
@@ -18,7 +20,7 @@ $db_connect = mysqli_connect($host, $user, $password, $dbname);
 if(!$db_connect){
     die("Ошибка подключения" . mysqli_connect_error());
 }
-
+//Проверка роли
 $roleCheck = "SELECT Role FROM user WHERE UserId = $acc";
 $roleCheckSql = mysqli_query($db_connect, $roleCheck);
 
@@ -26,11 +28,11 @@ if ($roleCheckSql && mysqli_num_rows($roleCheckSql) > 0) {
     $rowRole = mysqli_fetch_assoc($roleCheckSql);
     $RoleId = $rowRole['Role'];
 }
-
+//Проверка получения запроса
 if (isset($_GET['query'])) {
     $query = mysqli_real_escape_string($db_connect, $_GET['query']);
     $currentUserId = isset($_SESSION['acc']) ? $_SESSION['acc'] : null; // Получаем ID текущего пользователя
-
+    //Запрос на поиск пользователя по никнейму для администратора
     if($RoleId==1)
     {
         $q = "SELECT user.UserId,
@@ -52,6 +54,7 @@ if (isset($_GET['query'])) {
                    status.StatusPhoto 
           ORDER BY user.Scores DESC;";
     }
+    //Запрос на поиск пользователя по никнейму для читателя
     else
     {
         $q = "SELECT user.UserId,
@@ -77,6 +80,7 @@ if (isset($_GET['query'])) {
     $results = [];
     
     if ($sql) {
+        //Проверка подписок и блокировок
         while ($row = mysqli_fetch_assoc($sql)) {
             if($RoleId == 1){
                 $row['IsBlocked'] = $row['Block'] == 1;
